411b1b1fdcf24d4bd4d4d0fb11de607d24269afd
Peter Jenner <peterdavidjenner@gmail.com>
2021-03-04 14:04:00 +0000

    first commit

---
+++ src/main/java/com/bae/personalproject/QaPersonalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bae.personalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaPersonalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaPersonalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bae/personalproject/QaPersonalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bae.personalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaPersonalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
